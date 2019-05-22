993392982651a24f094b231fa2668a0b387dd9ed
Benjamin Clevenger <benhcleve@gmail.com>
2019-05-22 17:13:25 -0400

    first commit

---
+++ Assets/Code/ScoreManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    Dictionary<string, Dictionary<string, int> > userScores;
+
+    int changeCounter = 0;
+    void Start()
+    {
+        //Example Users. Takes a string for the username, then takes a type of 'Gems' and the amount of gems the user has.
+        SetScore("Ben", "Gems", 267);
+        SetScore("Peter", "Gems", 300);
+        SetScore("John", "Gems", 112);
+        SetScore("Bethany", "Gems", 4);
+        SetScore("Jordan" , "Gems", 16);
+        SetScore("Wendy" , "Gems", 19);
+        SetScore("Frank" , "Gems", 77);
+        SetScore("Patty" , "Gems", 47);
+
+    }
+
+    void Init() {
+        if(userScores != null)
+            return;
+
+        userScores = new Dictionary<string, Dictionary<string, int> >();
+    }
+
+
+
+
+    public int GetScore(string username, string scoreType) {
+        Init();
+
+        if(userScores.ContainsKey(username) == false) {
+            //We have no score record at all for this username
+            return 0;
+        }
+
+        if(userScores[username].ContainsKey(scoreType) == false) {
+            return 0;
+        }
+
+
+        return userScores[username][scoreType];
+    }
+
+    public void SetScore(string username, string scoreType, int value) {
+        Init();
+
+        changeCounter++;
+
+        if(userScores.ContainsKey(username) == false) {
+           userScores[username] = new Dictionary<string, int>();
+        }
+        userScores[username][scoreType] = value;
+    }
+
+    public void ChangeScore(string username, string scoreType, int amount) {
+        Init();
+        int currScore = GetScore(username, scoreType);
+        SetScore(username, scoreType, currScore + amount);
+
+    }
+
+     public string[] GetUserNames(string sortingScoreType) {
+        Init();
+
+        return userScores.Keys.OrderByDescending( n => GetScore( n, sortingScoreType)).ToArray();
+    }
+
+    public int GetChangeCounter() {
+        return changeCounter;
+    }
+}
---
+++ Assets/Code/UserScoreList.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UserScoreList : MonoBehaviour
+{
+    public GameObject UserEntryPrefab;
+    ScoreManager scoreManager;
+
+    int lastChangeCounter;
+
+    void Start()
+    {
+        scoreManager = GameObject.FindObjectOfType<ScoreManager>();
+        lastChangeCounter = scoreManager.GetChangeCounter();
+        }
+    void Update()
+    {
+        if(scoreManager == null) {
+            Debug.LogError("You forgot to add the score manager component to a game object!");
+            return;
+        }
+
+        if(scoreManager.GetChangeCounter() == lastChangeCounter) {
+            //No change since last update! Do nothing.
+            return;
+        }
+         lastChangeCounter = scoreManager.GetChangeCounter();
+
+        while(this.transform.childCount > 0) {
+            Transform c = this.transform.GetChild(0);
+            c.SetParent(null);
+            Destroy (c.gameObject);
+        }
+        //These names are pulled from Public ScoreManager function GetUserNames();
+        string[] names = scoreManager.GetUserNames("Gems");
+
+        //Finds Components for each UserEntry and changes text accordingly
+        foreach(string name in names) {
+            GameObject go = (GameObject)Instantiate(UserEntryPrefab);
+            int rank = System.Array.IndexOf(names ,name) + 1;
+            go.transform.SetParent(this.transform);
+            go.transform.Find ("UserName").GetComponent<TextMeshProUGUI>().text = name;
+            go.transform.Find ("UserScore").GetComponent<TextMeshProUGUI>().text = scoreManager.GetScore(name, "Gems").ToString();
+            go.transform.Find ("UserRank").GetComponent<TextMeshProUGUI>().text = rank.ToString();
+        }
+    }
+    
+}
